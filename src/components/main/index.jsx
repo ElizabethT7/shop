89e7b8315eaaa14f2fb32cf0494b16ebe94653b5
ElizabethT7 <tymofieievai@gmail.com>
2023-06-29 22:24:12 +0300

    feat: add cart

--- src/components/main/index.jsx
+++ src/components/main/index.jsx
@@ -2,10 +2,12 @@ import { useEffect, useState } from 'react';
 import { API_KEY, API_URL } from '../../config';
 import Preloader from '../preloader';
 import List from '../list';
+import Cart from '../cart';
 
 function Main() {
   const [items, setItems] = useState([]);
   const [loading, setLoading] = useState(true);
+  const [order, setOrder] = useState([]);
 
   useEffect(function getItems() {
     fetch(API_URL, {
@@ -23,7 +25,7 @@ function Main() {
 
   return (
     <main className="container content">
-      { 
+      <Cart quantity={order.length} />      { 
         loading ? <Preloader /> : <List items={items} />
       }
     </main>
